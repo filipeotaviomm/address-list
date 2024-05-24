8fd0d55eebba79e5b9a6dddda162463718da13e0
Filipe <filipeotaviomm@gmail.com>
2024-05-24 18:08:11 -0300

    feat: login router

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,9 +1,12 @@
 import "express-async-errors";
 import express, { Application, json } from "express";
 import { PrismaClient } from "@prisma/client";
+import { router } from "./routers/index.router";
 
 export const app: Application = express();
 
 export const prisma = new PrismaClient();
 
 app.use(json());
+
+app.use("/", router);
