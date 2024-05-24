1a5b958b81765ed827497e9b9b531acaa9bad43b
Filipe <filipeotaviomm@gmail.com>
2024-05-24 20:43:58 -0300

    feat: middlewares

---
+++ back/src/middlewares/users.middleware.ts
@@ -0,0 +1,74 @@
+import { User } from "@prisma/client";
+import { prisma } from "../app";
+import { verify } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors";
+
+export const isUserNameUnique = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): Promise<void> => {
+  if (!req.body.userName) return next();
+
+  const userName: User | null = await prisma.user.findUnique({
+    where: { userName: req.body.userName },
+  });
+
+  if (userName) {
+    throw new AppError("This username already exists");
+  }
+
+  return next();
+};
+
+export const doesUserExist = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const user: User | null = await prisma.user.findUnique({
+    where: { id: req.params.userId },
+  });
+
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
+
+  res.locals.user = user;
+};
+
+export const isUserLogged = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const authorization: string | undefined = req.headers.authorization;
+
+  if (!authorization) {
+    throw new AppError("Missing bearer token");
+  }
+
+  const token = authorization.split(" ")[1];
+
+  const decoded = verify(token, process.env.SECRET_KEY!);
+
+  res.locals.decoded = decoded;
+
+  return next();
+};
+
+export const doesUserHavePermission = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const userLoggedId = res.locals.decoded.userId;
+  const userIdUrl = req.params.userId;
+
+  if (userLoggedId !== userIdUrl) {
+    throw new AppError("You do not have permission");
+  }
+
+  return next();
+};
--- back/src/routers/user.router.ts
+++ back/src/routers/user.router.ts
@@ -3,13 +3,51 @@ import {
   createUserController,
   deleteUserController,
   getAllUsersController,
+  getUserByIdController,
   updateUserController,
 } from "../controllers/user.controller";
+import { validateBody } from "../middlewares/globals.middleware";
+import { userReqSchema, userUpdateSchema } from "../schemas/user.schema";
+import {
+  doesUserExist,
+  doesUserHavePermission,
+  isUserLogged,
+  isUserNameUnique,
+} from "../middlewares/users.middleware";
 
 export const userRouter: Router = Router();
 
-userRouter.post("/register", createUserController);
-userRouter.get("/all", getAllUsersController);
-userRouter.get("/:userId");
-userRouter.patch("/:userId", updateUserController);
-userRouter.delete("/:userId", deleteUserController);
+userRouter.post(
+  "/register",
+  validateBody(userReqSchema),
+  isUserNameUnique,
+  createUserController
+);
+
+userRouter.get("/all", isUserLogged, getAllUsersController);
+
+userRouter.get(
+  "/:userId",
+  doesUserExist,
+  isUserLogged,
+  doesUserHavePermission,
+  getUserByIdController
+);
+
+userRouter.patch(
+  "/:userId",
+  doesUserExist,
+  isUserLogged,
+  doesUserHavePermission,
+  validateBody(userUpdateSchema),
+  isUserNameUnique,
+  updateUserController
+);
+
+userRouter.delete(
+  "/:userId",
+  doesUserExist,
+  isUserLogged,
+  doesUserHavePermission,
+  deleteUserController
+);
