5767d4c1254101d8431ec61ca20b18d6e0cf0a65
Filipe <filipeotaviomm@gmail.com>
2024-05-24 19:19:39 -0300

    feat: user controller and router

--- back/src/routers/index.router.ts
+++ back/src/routers/index.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { loginRouter } from "./login.router";
+import { userRouter } from "./user.router";
 
 export const router: Router = Router();
 
 router.use("/login", loginRouter);
+router.use("/user", userRouter);
